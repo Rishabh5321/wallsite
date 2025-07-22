5b3de558c831eeb08a0787b8e7373952c194b338
Rishabh5321 <rishabh98818@outlook.com>
2025-07-22 13:25:02 +0530

    chore: update lighthouse config and add lighthouse script
    Updates lighthouse performance score and adds script.

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,11 +5,8 @@ module.exports = {
       url: ['http://localhost:8000']
     },
     assert: {
-      preset: 'lighthouse:recommended',
       assertions: {
-        'categories:performance': ['error', {minScore: 0.8}],
-        'errors-in-console': 'off',
-        'network-dependency-tree-insight': 'off',
+        'categories:performance': ['error', {minScore: 0.6}],
         'categories:accessibility': ['error', {minScore: 1}]
       }
     },
